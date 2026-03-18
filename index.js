6a9e7e119bd8c0727ae0272e9680a90a7f2633a5
NyarkohKwesi <132782414+NyarkohKwesi@users.noreply.github.com>
2026-03-18 20:37:00 +0000

    Delete button
    delete button is alive

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ onValue(referenceInDb, function(snapshot) {
         // Reset everything if the database is empty
         countEl.textContent = "0"
         tableBody.innerHTML = ""
-        deleteBtn.disabled = true
+        deleteBtn.disabled = false
     }
 })
 
